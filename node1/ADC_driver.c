1401aeab406d712d195391674b23110299f52631
erolfsnes <erlend.s.rolfsnes@gmail.com>
2023-09-21 14:23:04 +0200

    Added OLED driver

--- node1/ADC_driver.c
+++ node1/ADC_driver.c
@@ -5,6 +5,8 @@
 #include "ADC_header.h"
 #define BASE_ADDRESS_ADC 0x1400;
 
+volatile char *ext_mem_adc = (char*) BASE_ADDRESS_ADC;
+
 void ADC_Init(void)
 {
 	// Define PD4 as output pin
@@ -13,29 +15,25 @@ void ADC_Init(void)
 	// Configure Time/Counter Control Register to PWM mode
 	TCCR3A = (1 << WGM30) | (1 << COM3A1) | (1 << COM3A0); 
 	TCCR3B = (1 << CS10);
-	OCR3A = 127;
+	OCR3A = 127; // When timer's count value reaches the OCR-value, the PWM-output is changed
 	
 	// Enable interrupt
 	sei();
 }
 
-void ADC_Read(adc_data *data)
+void ADC_Read(volatile adc_data *data)
 {
-	volatile char *ext_mem = (char*) BASE_ADDRESS_ADC;
-	
 	// First write a value to the ADC to set the write signal low
-	ext_mem[0] = 0;
+	ext_mem_adc[0] = 0;
 	
 	// Wait for conversion to finish (conversion time = 3.8 us)
 	_delay_ms(10);
 	
 	// Obtain the converted value for each channel and store it in the struct
-	data->ch1 = ext_mem[0]; // Joystick X-axis
-	data->ch2 = ext_mem[0]; // Joystick Y-axis
-	data->ch3 = ext_mem[0]; // Left slider
-	data->ch4 = ext_mem[0]; // Right slider
-	
-	//printf("%d\t %d\t %d\t %d\t\r\n", data->ch1, data->ch2, data->ch3, data->ch4);
+	data->ch1 = ext_mem_adc[0]; // Joystick X-axis
+	data->ch2 = ext_mem_adc[0]; // Joystick Y-axis
+	data->ch3 = ext_mem_adc[0]; // Left slider
+	data->ch4 = ext_mem_adc[0]; // Right slider
 }
 
 // rel. between voltage and joystick angle: f = 40x - 100 (x = voltage)
@@ -58,14 +56,14 @@ void pos_calibrate(int16_t *x_offs, int16_t *y_offs)
 
 pos_t pos_read(adc_data *data)
 {	
+	// Transform joystick position data to be in the interval (-100, 100) instead of (0, 255)
 	int32_t x_pos = (int16_t) data->ch1 - data->x_offs;
 	int32_t y_pos = (int16_t) data->ch2 - data->y_offs;
 	int16_t norm_pos_x = (x_pos * 200) / 255 - 100;
 	int16_t norm_pos_y = (y_pos * 200) / 255 - 100;
 	//int32_t norm_pos_x = ((int32_t)(data->ch1) * (127 - (-127)) / 255-27) + (-127);
-
-	//printf("%d\r\n", data->ch1);
 	
+	// Store transformed x and y data in a struct and return it
 	pos_t pos_data;
 	pos_data.x = norm_pos_x;
 	pos_data.y = norm_pos_y;
@@ -75,14 +73,17 @@ pos_t pos_read(adc_data *data)
 
 joystick_dir dir_read(pos_t *pos_data)
 {
+	// Return joystick direction based on a certain threshold in x and y position
+	uint8_t thold = 15;
+	
 	if (abs(pos_data->x) > abs(pos_data->y)) {
-		if (pos_data->x < -15) return LEFT;
-		else if (pos_data->x > 15) return RIGHT;
+		if (pos_data->x < -thold) return LEFT;
+		else if (pos_data->x > thold) return RIGHT;
 		else return NEUTRAL;
 	}
 	else{
-		if (pos_data->y < -15) return DOWN;
-		else if (pos_data->y > 15) return UP;
+		if (pos_data->y < -thold) return DOWN;
+		else if (pos_data->y > thold) return UP;
 		else return NEUTRAL;
 	}
 }
